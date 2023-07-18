fb8b5f7a9cecaeaf6babd900f6e886302d56372c
luongna <anhluong7101@gmail.com>
2023-07-18 19:47:52 +0700

    gắn token vào axios

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -2,24 +2,7 @@ import axios from 'axios';
 import { store } from '~/utils/store/store';
 const BASE_URL = 'http://localhost:8080';
 
-// axios.interceptors.request.use(async (config) => {
-//     const customHeaders = {};
-//     const state = store.getState();
-//     const accessToken
-//     if (state) {
-//         const accessToken = state.auth.login?.currenUser.accessToken;
-//         customHeaders.authorization = `Bearer ${accessToken}`;
 
-//     }
-
-//     return {
-//         ...config,
-//         headers: {
-//           authorization: `Bearer ${accessToken}`,
-//             ...config.headers, // but you can override for some requests
-//         },
-//     };
-// });
 const api = axios.create({
     baseURL: BASE_URL,
     headers: {
